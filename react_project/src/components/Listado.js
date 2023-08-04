624635bbf115bac6f6f0b595a135688ae15e5e7d
Gonza <gonl@gej.com>
2023-08-04 13:20:51 +0200

    fat: aplica funcionalidad y evento al boton borrar, se observa funcionamiento ok.

--- react_project/src/components/Listado.js
+++ react_project/src/components/Listado.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 
-export const Listado = ({listadoState, setListadoState}) => {
+export const Listado = ({ listadoState, setListadoState }) => {
 
   //const [listadoState, setListadoState] = useState([]);
 
@@ -20,6 +20,26 @@ export const Listado = ({listadoState, setListadoState}) => {
     let peliculas = JSON.parse(localStorage.getItem("pelis"));
 
     setListadoState(peliculas);
+
+    return peliculas;
+  }
+
+  //funcion para borrar pelicula
+
+  const borrarPeli = (id) => {
+
+   //conseguir peliculas almacenadas
+   let pelis_almacenadas = conseguirPeliculas();
+
+   //filtrar esas peliculas para que elimine del array la que no quiero
+   let nuevo_array_pelis = pelis_almacenadas.filter(peli => peli.id !== parseInt(id));
+
+   //actualizar estado del listado
+   setListadoState(nuevo_array_pelis);
+
+   //actualizar los datos en el localStorage
+   localStorage.setItem("pelis", JSON.stringify(nuevo_array_pelis));
+
   }
 
   return (
@@ -30,12 +50,12 @@ export const Listado = ({listadoState, setListadoState}) => {
             <h3 className="title">{peli.titulo}</h3>
             <p className="description">{peli.descripcion}</p>
             <button className="edit">Editar</button>
-            <button className="delete">Borrar</button>
+            <button className="delete" onClick={() => borrarPeli(peli.id)}>Borrar</button>
           </article>
         );
       })
-      : <h2>No hay peliculas para mostrar</h2>
-    }
+        : <h2>No hay peliculas para mostrar</h2>
+      }
     </>
   )
 }
