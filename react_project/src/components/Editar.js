051d64c4fe74396f3ffce76c6f21549596498ae4
Gonza <gonl@gej.com>
2023-08-04 15:00:36 +0200

    feat: Diseña formulario para editar la pelicula en la lista correspondiente.

--- react_project/src/components/Editar.js
+++ react_project/src/components/Editar.js
@@ -1,7 +1,30 @@
 import React from 'react'
 
 export const Editar = () => {
-  return (
-    <div>Editar</div>
-  )
+
+    const titulo_componente = "Editar pelicula"
+
+    return (
+        <div className='edit_form'>
+            <h3 className='title'>{titulo_componente}</h3>
+            <form>
+                <input
+                    type="text"
+                    name="titulo"
+                    className="titulo_editado"
+                    placeholder='ghola'
+                    defaultValue="Titulo original de la pelicula" />
+
+                <textarea
+                    name="descripcion"
+                    defaultValue="Descripcion original"
+                    className="descripcion_editada" />
+
+                <input
+                    type="submit"
+                    className="editar"
+                    value="Actualizar" />
+            </form>
+        </div>
+    )
 }
