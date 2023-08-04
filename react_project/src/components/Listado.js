37881aa8f462a7548ed757b268cf029a752a8db7
Gonza <gonl@gej.com>
2023-08-04 11:17:02 +0200

    feat: Actualiza funcionalidad del componente Listado y ensea por pantalla la informacion del localStorage.

--- react_project/src/components/Listado.js
+++ react_project/src/components/Listado.js
@@ -1,35 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
-    return (
-        <>
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">GonzaloPelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
 
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">GonzaloPelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+  const [listadoState, setListadoState] = useState([]);
 
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">GonzaloPelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+  useEffect(() => {
 
-            <article className="peli-item">
-                <h3 className="title">Desarrollo Web</h3>
-                <p className="description">GonzaloPelicula</p>
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
-        </>
-    )
+    console.log("cmponentes del listado de peliculas cargado!!");
+    conseguirPeliculas();
+
+  }, []);
+
+
+
+  const conseguirPeliculas = () => {
+
+    let peliculas = JSON.parse(localStorage.getItem("pelis"));
+
+    setListadoState(peliculas);
+  }
+
+  return (
+    <>
+      {listadoState != null ? listadoState.map(peli => {
+        return (
+          <article key={peli.id} className="peli-item">
+            <h3 className="title">{peli.titulo}</h3>
+            <p className="description">{peli.descripcion}</p>
+            <button className="edit">Editar</button>
+            <button className="delete">Borrar</button>
+          </article>
+        );
+      })
+      : <h2>No hay peliculas para mostrar</h2>
+    }
+    </>
+    
+  )
 }
